ee32a6aac2e261d3045c94945cbd94f0d58c654c
Denis Kisina <kisinad@gmail.com>
2020-08-04 22:50:41 -0500

    added nest json object and array of data to mapped  pojo

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -15,10 +15,24 @@ public class Driver {
 
 			// read json from file and map/connect to java pojo
 
-			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			// print student first and last name
 
 			System.out.println("Student First Name " + theStudent.getFirstName());
 			System.out.println("Student last Name " + theStudent.getLastName());
+
+			// printt address street and city
+
+			Address theAddress = theStudent.getAddress();
+
+			System.out.println("Street: " + theAddress.getStreet());
+
+			System.out.println("City: " + theAddress.getCity());
+
+			for (String string : theStudent.getLanguages()) {
+				System.out.println(string);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
